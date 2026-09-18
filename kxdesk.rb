e33be2a6f7c5b561a3d47852930c0e3c5d83e9b0
Kevin Dreßler <mail@kxfin.xyz>
2026-09-18 22:23:03 +0200

    kxdesk: v0.1.19

--- kxdesk.rb
+++ kxdesk.rb
@@ -2,8 +2,8 @@ class Kxdesk < Formula
   desc "Personal desktop daemon for SketchyBar, yabai and friends"
   homepage "https://github.com/kvndrsslr/kxdesk"
   url "https://github.com/kvndrsslr/kxdesk.git",
-      tag:      "v0.1.18",
-      revision: "9a7e4d20a183c8200ba20f97d38e8b88622e054c"
+      tag:      "v0.1.19",
+      revision: "07c04805c9ecd7e235653d71ad8aa9e1211de7fb"
   head "https://github.com/kvndrsslr/kxdesk.git", branch: "main"
 
   depends_on "terminal-notifier"
